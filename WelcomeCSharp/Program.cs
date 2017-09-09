1d0856dd40abc69a239efbe2d0266cb367414ae2
Christopher Stone <christopher@closetoeuphoria.com>
2017-09-08 20:15:25 -0400

    C# - Spec v1 - Use console input in an interpolated string

---
+++ WelcomeCSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WelcomeCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello and Welcome to C#!");
+            Console.Write("Please tell me your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+            Console.ReadLine();
+        }
+    }
+}
