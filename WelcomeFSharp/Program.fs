db5ad469949f4881e9e30ef76bf7d8085513b6ce
Christopher Stone <christopher@closetoeuphoria.com>
2017-09-08 20:10:47 -0400

    F# - Spec v1 - Use console input in a formatted string

---
+++ WelcomeFSharp/Program.fs
@@ -0,0 +1,10 @@
+﻿open System
+
+[<EntryPoint>]
+let main _argv = 
+    printfn "Hello and Welcome to F#!"
+    printf "Please tell me your name: "
+    let name = Console.ReadLine()
+    printfn "Hello, %s!" name
+    Console.ReadLine() |> ignore
+    0
