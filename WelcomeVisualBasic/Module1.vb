e46a59cf4e7855267a826a2dee8de1c01c4c8676
Christopher Stone <christopher@closetoeuphoria.com>
2017-09-08 20:23:15 -0400

    Visual Basic - Spec v1 - Use console input in an interpolated string

---
+++ WelcomeVisualBasic/Module1.vb
@@ -0,0 +1,9 @@
+﻿Module Module1
+    Sub Main()
+        Console.WriteLine("Hello and Welcome to Visual Basic!")
+        Console.Write("Please tell me your name: ")
+        Dim Name As String = Console.ReadLine()
+        Console.WriteLine($"Hello, {Name}!")
+        Console.ReadLine()
+    End Sub
+End Module
